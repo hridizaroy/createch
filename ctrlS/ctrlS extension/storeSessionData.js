8dc7a42e543abc792f8311de64d338374d9b9292
hridizaroy <52705197+hridizaroy@users.noreply.github.com>
2021-06-03 01:19:06 +0530

    Delete ctrlS directory

--- ctrlS/ctrlS extension/storeSessionData.js
+++
@@ -1,8 +0,0 @@
-
-var s_user = document.getElementById('s_user').innerText;
-var s_team = document.getElementById('s_team').innerText;
-var s_project = document.getElementById('s_project').innerText;
-
-chrome.storage.sync.set({ s_user : s_user });
-chrome.storage.local.set({ s_team: s_team });
-chrome.storage.local.set({ s_project: s_project });
--- ctrlS/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$host = 'localhost';
-$user = 'hridizaroy';
-$pass = 'Rinik123';
-$db = 'accountsctrlS';
-$mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
